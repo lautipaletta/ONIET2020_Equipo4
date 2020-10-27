49f3d62f812676f5be31fcbec55ed46c5674e8d1
TorresUser <torres.kilmes@gmail.com>
2020-10-27 17:04:16 -0300

    API

---
+++ API/api.py
@@ -0,0 +1,41 @@
+import requests
+import datetime
+import json
+
+def pedirDatosUlt(pais):
+    url = "https://api.covid19api.com/summary"
+    payload = {}
+    headers= {}
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    todos = json.loads(response.text)
+    paises = todos.get("Countries")
+    for pais in paises:
+        if pais.get("Country") == pais:
+            return pais
+
+def pedirDatosDias(slug):
+    variable_to = datetime.datetime.now() 
+    variable_from = variable_to - datetime.timedelta(days = 11)
+    variable_to = str(variable_to).replace(" ", "T")
+    variable_from = str(variable_from).replace(" ", "T")
+    variable_to = variable_to.split('.')
+    variable_from= variable_from.split('.')
+
+    url = "https://api.covid19api.com/country/{}?from={}Z&to={}Z".format(slug, variable_from[0], variable_to[0])
+    payload = {}
+    headers= {}
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    dias = json.loads(response.text)
+    return dias
+
+def pedirPaises():
+    url = "https://api.covid19api.com/https://api.covid19api.com/countriescountries"
+    payload = {}
+    headers= {}
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    paises = json.loads(response.text)
+    return paises
+
