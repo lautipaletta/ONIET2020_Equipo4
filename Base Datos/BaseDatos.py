793ea47d02de7b32b60c8472da961874c155ad30
JuanICasareski <juani.casareski@gmail.com>
2020-10-27 17:05:13 -0300

    logica base de datos.

--- Base Datos/BaseDatos.py
+++ Base Datos/BaseDatos.py
@@ -5,44 +5,72 @@ cursor = connection.cursor()
 
 
 cmd = """CREATE TABLE IF NOT EXISTS APPCOVID(
-            first TEXT NOT NULL ,
+            first TEXT NOT NULL,
             last TEXT NOT NULL,
             email TEXT NOT NULL PRIMARY KEY,
             password BLOB NOT NULL,
             country TEXT NOT NULL,
-            countrycode TEXT NOT NULL,
+            slug TEXT NOT NULL,
+            residence TEXT NOT NULL,
             lastlogin DATE
     )"""
 cursor.execute(cmd)
 
 
-def CambiarPassword(nextPassword, thisUser):
-    cmd = f""" UPDATE APPCOVID SET password = {nextPassword} WHERE email LIKE {thisUser} """ 
-    cursor.execute(cmd)
-    connection.commit()
 
-def CambiarFirst(nextFirst, thisUser):
-    cmd = f""" UPDATE APPCOVID SET password = {nextFirst} WHERE email LIKE {thisUser} """
-    cursor.execute(cmd)
-    connection.commit()
+
+#.pack_forget() PARA ESCONDER VENTANA
+
+
+
+def ChangePassword(nextPassword, thisUser):
+    if not nextPassword:
+        cmd = f""" UPDATE APPCOVID SET password = {nextPassword} WHERE email == {thisUser} """ 
+        cursor.execute(cmd)
+        connection.commit()
+
+def ChangeFirst(nextFirst, thisUser):
+    if not nextFirst:
+        cmd = f""" UPDATE APPCOVID SET first = {nextFirst} WHERE email == {thisUser} """
+        cursor.execute(cmd)
+        connection.commit()
     
-def CambiarLast(nextLast, thisUser):
-    cmd = f""" UPDATE APPCOVID SET password = {nextLast} WHERE email LIKE {thisUser} """
-    cursor.execute(cmd)
-    connection.commit()
+def ChangeLast(nextLast, thisUser):
+    if not nextLast:
+        cmd = f""" UPDATE APPCOVID SET last = {nextLast} WHERE email == {thisUser} """
+        cursor.execute(cmd)
+        connection.commit()
 
-def CambiarEmail(nextEmail, thisUser):
-    cmd = f""" UPDATE APPCOVID SET password = {nextEmail} WHERE email LIKE {thisUser} """
-    cursor.execute(cmd)
-    connection.commit()
+def ChangeCountry(nextCountry, thisUser):
+    if not nextCountry:
+        cmd = f""" UPDATE APPCOVID SET country = {nextCountry} WHERE email == {thisUser} """
+        cursor.execute(cmd)
+        connection.commit()    
 
-def CambiarCountry(nextCountry, thisUser):
-    cmd = f""" UPDATE APPCOVID SET password = {nextCountry} WHERE email LIKE {thisUser} """
-    cursor.execute(cmd)
-    connection.commit()    
+def ChangeSlug(nextSlug, thisUser):
+    if not nextSlug:
+        cmd = f""" UPDATE APPCOVID SET slug = {nextSlug} WHERE email == {thisUser} """
+        cursor.execute(cmd)
+        connection.commit()   
 
-def CambiarCountryCode(nextCountryCode, thisUser):
-    cmd = f""" UPDATE APPCOVID SET password = {nextCountryCode} WHERE email LIKE {thisUser} """
+def ChangeResidence(nextResidence, thisUser):
+    if not nextResidence:
+        cmd = f""" UPDATE APPCOVID SET residence = {nextResidence} WHERE email == {thisUser} """
+        cursor.execute(cmd)
+        connection.commit()   
+
+
+
+
+
+def ValidateLogin(Email, Password):
+    cmd = f""" SELECT (password) FROM APPCOVID WHERE email = {Email} """
     cursor.execute(cmd)
-    connection.commit()    
+    findedPassword = cursor.fetchone()
+
+    if Password == findedPassword:
+        
+    else
+        print("La contraseña no corresponde o no está permitida.")
+
 
